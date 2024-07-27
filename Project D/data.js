1d2228654fe455ae2b0b29ae9fd584084eeaee21
lankabhavani <115463765+lankabhavani@users.noreply.github.com>
2024-07-27 23:41:31 +0530

    Add files via upload

---
+++ Project D/data.js
@@ -0,0 +1,28 @@
+let movies = [
+    {
+        name:'Godzilla X Kong',
+        des:'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+        image:'DPhotos/slide-1.jpg'
+    },
+    {
+        name:'Avatar',
+        des:'ABCD dolor sit amet consectetur adipisicing elit.',
+        image:'DPhotos/slide-4.png'
+    },
+    {
+        name:'Resident Evil',
+        des:'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+        image:'DPhotos/slide-3.png'
+    },
+    {
+        name:'Chhote Miyan Bade Miyan',
+        des:'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+        image:'DPhotos/slide-2.jpg'
+    },
+    {
+        name:'Godzilla X Kong2',
+        des:'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+        image:'DPhotos/slide-5.png'
+    }
+
+]
